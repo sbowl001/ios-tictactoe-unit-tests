be4760f294171c1326e65e0ab50f722c60a5079a
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-18 20:25:33 +0800

    ai tests

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -110,7 +110,28 @@ class GameAITests: XCTestCase {
     }
     
     func testIncompleteGame() {
+        /*
+         x o o
+         o x x
+         - x o
+         */
+        var board = GameBoard()
+      
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (2, 1))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .o, on: (2, 2))
+        try! board.place(mark: .x, on: (1, 2))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertFalse(board.isFull)
+    
     }
+    
 
     func testCatsGame() {
         var board = GameBoard()
@@ -131,5 +152,6 @@ class GameAITests: XCTestCase {
 
         XCTAssertFalse(game(board: board, isWonBy: .o))
         XCTAssertFalse(game(board: board, isWonBy: .x))
+        XCTAssertTrue(board.isFull)
     }
 }
